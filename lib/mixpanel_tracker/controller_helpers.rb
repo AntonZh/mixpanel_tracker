235841b3fe0d1d9f94cca706157ea9527a453296
azhavoronkov <anton.zhavoronkov@gmail.com>
2014-01-16 18:22:38 +0400

    Events

--- lib/mixpanel_tracker/controller_helpers.rb
+++ lib/mixpanel_tracker/controller_helpers.rb
@@ -1,7 +1,8 @@
 module MixpanelTracker
   module ControllerHelpers
-    def track_event(name)
-      session[:mixpanel_events] = name
+    def track_event(name, options = {})
+      session[:mixpanel_events] ||= []
+      session[:mixpanel_events] << { name: name, options: options }
     end
   end
 end
